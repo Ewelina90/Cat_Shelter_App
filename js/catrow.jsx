d40f6be3a11778c11b6b46d75c8896ff2fb43a33
Ewelina Dziura <ewedziura@gmail.com>
2017-06-26 19:18:22 +0200

    CatRow component

---
+++ js/catrow.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CatRow extends React.Component {
+
+    handleImgClick = (picture,catName) => {
+        if(typeof this.props.onPictureClick === 'function'){
+            this.props.onPictureClick(picture,catName);
+        }
+    };
+
+    render() {
+        const name = this.props.kitty.likesKids ?
+        this.props.kitty.name : <span style={{color:'red'}}> {this.props.kitty.name}</span> ;
+
+        return(
+            <tr>
+                <td><img style={{width:'30px', height:'30px', borderRadius: '50%' }}
+                    src={this.props.kitty.picture}
+                    onClick={e => this.handleImgClick(this.props.kitty.picture ,this.props.kitty.name)}/>
+
+                </td>
+                <td>{name}</td>
+                <td>{this.props.kitty.age}</td>
+            </tr>
+        )
+    }
+}
+
+export default CatRow;
