e16f81e8d713ebfd987718c2d7d4c49de99e88eb
Ewelina Dziura <ewedziura@gmail.com>
2017-06-26 19:10:57 +0200

    App component

---
+++ js/workshops.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './searchbar.jsx';
+import CatTable from './cattable.jsx';
+import CatBasket from './catbasket.jsx';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    class App extends React.Component {
+
+        constructor(props) {
+            super(props);
+            this.state = {
+                filterText : '',
+                likesKids : false,
+                picture : '',
+                clickOff : true,
+                currentCatName : '',
+            };
+        }
+
+        handleBoxCheck = (event) => {
+            this.setState({
+                likesKids : !this.state.likesKids,
+            });
+        };
+
+        handleSearchChange = (value) => {
+            this.setState({
+                filterText : value,
+            });
+        };
+
+        handlePictureClick = (catPicture,catName) => {
+            this.setState({
+                picture : catPicture,
+                currentCatName : catName,
+                clickOff: false,
+            });
+        };
+
+        handleClickOf = (event) => {
+            this.setState({
+                picture : '',
+                clickOff: true,
+            });
+        };
+
+        render() {
+            if(this.state.picture){
+
+                return(
+                    <CatBasket picture={this.state.picture} currentCatName={this.state.currentCatName} onImgClick={this.handleClickOf}/>
+                )
+            }else if(this.state.clickOff){
+                return (
+                    <div className="app">
+                        <SearchBar filterText={this.state.filterText} likesKids={this.state.likesKids} onChange={this.handleBoxCheck} onFilter={this.handleSearchChange}/>
+                        <CatTable kitties={this.props.kitties} filterText={this.state.filterText} likesKids={this.state.likesKids} onPictureClick={this.handlePictureClick}/>
+                    </div>
+                )
+            }
+        }
+    }
+
+    const kitties = [
+     {category: "Male", age: "4", likesKids: true, name: "Fidel Catstro", picture:'./images/cat_1.jpg'},
+     {category: "Male", age: "9", likesKids: true, name: "Hairy Potter",picture:'./images/cat_2.jpg'},
+     {category: "Male", age: "2", likesKids: false, name: "Grumpy", picture:'./images/cat_3.jpg'},
+     {category: "Female", age: "1", likesKids: true, name: "Jude Paw", picture:'./images/cat_4.jpg'},
+     {category: "Female", age: "2", likesKids: false, name: "Lucifurr", picture:'./images/cat_5.jpg'},
+     {category: "Female", age: "3", likesKids: true, name: "Meowly Cyrus", picture:'./images/cat_6.jpg'}
+    ];
+
+ReactDOM.render(
+  <App kitties={kitties} />,
+  document.getElementById('app')
+)
+});
